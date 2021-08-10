9a3d58795730f37fd71eb6ce6b27f4afabe8ddf6
Henrique Passarelli <henrique.passarellicc@gmail.com>
2021-08-10 18:30:49 -0300

    working

--- script.js
+++ script.js
@@ -26,45 +26,105 @@ pizzaJson.forEach(pizza => {
     document.querySelector(".content").appendChild(container);
 
 });
-
+let index;
 function getPizza(event) {
     let pizzaId = (event.target.id) - 1;
-    return Pizza.modal(pizzaId);
+    index = pizzaId;
+    return Modal.open(pizzaId), index;
 }
 
-const Pizza = {
+const Modal = {
 
-    modal(index) {
+    open(index) {
         let flavor = pizzaJson[index];
         let modal = document.querySelector('.modal');
         modal.style.display = 'flex';
         modal.innerHTML = `
             <div class="modal-container">
-            <div class="flavor">
-                
-                <img src="${flavor.img}" >
-            </div>
-            <div class="aside"> 
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+                <div class="flavor">                    
+                    <img src="${flavor.img}" >
                 </div>
-                <h1>${flavor.name}</h1>
-                <p>${flavor.description}</p>
-                <div class="size">
-                    <a>Small</a>
-                    <a>Regular</a>
-                    <a>Large</a>
+                <div class="aside"> 
+                    <div class="close-container">
+                        <svg id="close" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+                    </div>
+                    <h1>${flavor.name}</h1>
+                    <p>${flavor.description}</p>
+                    <div class="size">
+                        <a id="size01" >Small</a>
+                        <a id="size02">Regular</a>
+                        <a id="size03">Large</a>
 
+                    </div>
+                    <div class="price">
+                        <h2 >R$ <span id="price">${flavor.price.toFixed(2)}</span></h2>
+                        <div class="amount-container">
+                            <svg class="minus" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13H5v-2h14v2z"/></svg>
+                            <span class="amount" value="01">01</span>
+                            <svg class="plus" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
+                        </div>
+                    </div>
+                    <div class="addTo">
+                        <a id="addTo">Adicionar ao carrinho</a>
+                    </div>
+                
                 </div>
-                <div class="price">
-                    <h2>${flavor.price}</h2>
-                </div>
-            
-            </div>
             </div>
         `
 
-        console.log(pizzaJson[index]);
+        document.getElementById('close').addEventListener('click', Modal.close);
+        document.querySelector('.minus').addEventListener('click', Modal.decrease);
+        document.querySelector('.plus').addEventListener('click', Modal.increase);
+
+    },
+
+    increase() {
+        let element = document.querySelector('.amount');
+        let value = parseInt(element.innerHTML);
+        value += 1;
+        if (value < 10) {
+            value = "0" + value;
+        }
+        element.innerHTML = value.toString();
+        Modal.price(value);
+    },
+
+    decrease() {
+        let element = document.querySelector('.amount');
+        let value = parseInt(element.innerHTML);
+        if (value <= 1) {
+            value = 1;
+        } else {
+            value -= 1;
+        }
+
+        if (value < 10) {
+            value = "0" + value;
+        }
+        element.innerHTML = value.toString();
+        Modal.price(value);
+    },
+
+    price(amount) {
+        let value = pizzaJson[index].price.toFixed(2);
+        let price = (value * amount).toFixed(2);
+        value = price.toString();
+        document.querySelector('#price').innerHTML = value;
+        console.log(value);
+    },
+
+    close() {
+        document.querySelector('.modal').style.display = "none";
+    }
+}
+
+const Cart = {
+
+    addTo() {
+
+    },
+
+    removeFrom() {
 
     }
 }
